42cd9637da31383fdff1458a363400ae02044170
sofie peeters <you@example.com>
2021-06-17 12:50:51 +0200

    start with breadthfirst

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 from code.classes import protein, aminoacid
 from code.visualisation import visualise
-from code.algorithms import randomise, greedy
+from code.algorithms import randomise, greedy, breadthfirst
 import copy
 import csv
+import time
 
 if __name__ == "__main__":
     # promt user for sequence
     data = str(input("Insert sequence here: "))
 
-    algorithm = "greedy"
-    # while algorithm != "randomise" or "greedy":
-    #     algorithm = str(input("Which algorithm would you like to use? [randomise, greedy]: "))
-
+    algorithm = "breadthfirst"
+   
     times = int(input("How many times do you want to run the algorithm? "))
 
     csv_rows_baseline = []
     highest_stability = 1
     
+    start_time = time.time()
+
     for protein_counter in range(0, times):
 
         # print at every 100 proteins
         if protein_counter%100 == 0:
-            print("making protein nr", protein_counter)
+            print("Making protein nr", protein_counter)
+            print("Runtime: %s seconds" % (time.time() - start_time))
 
-        # execute random or greedy algorithm
+        # execute random/greedy/breadthfirst algorithm
         if algorithm == "randomise":
-            # Random
             random_object = randomise.Randomise(data)
             current_protein = random_object.protein
         elif algorithm == "greedy":
-            # Greedy
             greedy_object = greedy.Greedy(data)
             current_protein = greedy_object.protein
+        elif algorithm == "breadthfirst":
+            breadthfirst_object = breadthfirst.Breadthfirst(data)
+            current_protein = breadthfirst_object.protein
 
         # count score
         #print("calculate score...")
